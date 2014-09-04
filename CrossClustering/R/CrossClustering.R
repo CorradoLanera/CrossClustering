f00ea495a0a42a2032705badccee815557ddaee0
michele <mic.cipi@gmail.com>
2014-09-04 16:29:14 -0400

    added documentation for the CrossClustering function

--- CrossClustering/R/CrossClustering.R
+++ CrossClustering/R/CrossClustering.R
@@ -1,3 +1,42 @@
+#' CrossClustering: a clustering algorithm with automatic estimation of the number of clusters and identification of outliers
+#'
+#' This function performs the CrossClustering algorithm. This method combines the ward and complete linkage algorighms, providing automatic estimation of a suitable number of clusters and identification of outlier elements.
+#'
+#' @param d a dissimilarity structure as produced by the function \code{dist}
+#' @param k.w.min minimum number of clusters for the Ward's minimum variance method. By default is set equal 2
+#' @param k.w.max maximum number of clusters for the Ward's minimum variance method (see details)
+#' @param k.c.max maximum number of clusters for the Complete-linkage method. It can not be equal or greater than the number of elements to cluster (see details)
+#' @param out logical. If \code{TRUE} (default) outliers must be searched (see details)
+#' @return A list of objects describing characteristics of the partitioning as follows:
+#' \item{Optimal.cluster}{number of clusters}
+#' \item{Cluster.list}{a list of clusters; each element of this lists contains the indices of the elemenents belonging to the cluster}
+#' \item{Silhouette}{the average silhouette witdh over all the clusters}
+#'  
+#'  @details See cited document for more details.
+#'  @examples
+#'  
+#' ### Generate simulated data
+#' toy=matrix(NA, nrow=10, ncol=7)
+#' colnames(toy)=paste("Sample", 1:ncol(toy), sep="")
+#' rownames(toy)=paste("Gene", 1:nrow(toy), sep="")
+#' set.seed(123)
+#' toy[,1:2] <- rnorm(n=nrow(toy)*2, mean=10, sd=0.1)
+#' toy[,3:4] <- rnorm(n=nrow(toy)*2, mean=20, sd=0.1)
+#' toy[,5:6] <- rnorm(n=nrow(toy)*2, mean=5, sd=0.1)
+#' toy[,7] <- runif(n=nrow(toy), min=0, max=1)
+#' 
+#' ### toy is transposed as we want to cluster samples
+#' d <- dist(t(toy), method="euclidean")
+#' 
+#' ### Run CrossClustering
+#' CrossClustering(d, k.w.min=2, k.w.max=5, k.c.max=6, out=TRUE)
+#'  
+#'  @author
+#'  Paola Tellaroli, \email{tellaroli@@stat.unipd.it}; Marco Bazzi, \email{bazzi@@stat.unipd.it}; Michele Donato, \email{michele.donato@@wayne.edu}
+#'  
+#'  @references
+#'  Tellaroli, P., Bazzi, M., Brazzale, A. R., Donato, M., Draghici, S. Cross Clustering: a partial clustering algorithm with automatic estimation of the number of clusters (manuscript in preparation)
+
 CrossClustering<-function(d,k.w.min,k.w.max,k.c.max,out=TRUE)
 {
   require(cluster)
@@ -23,8 +62,7 @@ CrossClustering<-function(d,k.w.min,k.w.max,k.c.max,out=TRUE)
                                             beta.clu.complete=beta.clu.complete, return.list=T)
     clustz <- sapply(1:n, geneinlista, cluster.list$beta.list)
   }else{
-    cluster.list <- apply(k.star, 1, max.proportion.function, beta.clu.ward=beta.clu.ward, 
-                          beta.clu.complete=beta.clu.complete, return.list=T)
+    cluster.list <- apply(k.star, 1, max.proportion.function, beta.clu.ward=beta.clu.ward, beta.clu.complete=beta.clu.complete, return.list=T)
     clustz <- sapply(cluster.list, function(lasim) sapply(1:n, geneinlista, lista=lasim$beta.list)) 
     
   }
