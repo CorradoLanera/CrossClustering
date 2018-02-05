de4de0cff9581c3f9d088cb8439a0bb028ddcfdc
Corrado Lanera (xps) <corrado.lanera@gmail.com>
2018-02-05 11:22:09 -0500

    test-ARI_contingency

--- R/ARI_contingency.R
+++ R/ARI_contingency.R
@@ -1,8 +1,12 @@
 #' Computes the adjusted Rand index and the confidence interval, comparing
 #' two classifications from a contingency table.
 #'
-#' @param mat The contingency table
-#' @param alpha The significance level
+#' @param mat A matrix of integers representing the contingency table of
+#'   reference
+#' @param alpha A single number strictly included between 0 and 1
+#'   representing the significance level of interest. (default is 0.05)
+#' @param digits An integer for the returned significant digits to return
+#'   (default is 2)
 #'
 #' @details
 #' The adjusted Rand Index (ARI) should be interpreted as follows:
@@ -25,7 +29,7 @@
 #'
 #' @examples
 #' mat <- matrix(c(4,5,3,3,8,4),ncol=3,byrow = TRUE)
-#' ARI_contingency(mat, alpha = 0.05)
+#' ARI_contingency(mat)
 #'
 #' @author
 #' Paola Tellaroli, <paola [dot] tellaroli [at] unipd [dot] it>;;
@@ -40,13 +44,28 @@
 #' D. Steinley (2004) Properties of the Hubert-Arabie Adjusted Rand Index,
 #' Psychological Methods, 9(3), 386-396
 
-ARI_contingency <- function(mat, alpha){
+ARI_contingency <- function(mat, alpha = 0.05, digits = 2){
+  assertive::assert_is_matrix(mat)
+  assertive::assert_is_numeric(mat)
+  assertive::assert_all_are_positive(mat)
+  assertive::assert_all_are_equal_to(mat, as.integer(mat))
+  assertive::assert_is_a_double(alpha)
+  assertive::assert_all_are_proportions(alpha,
+    lower_is_strict = TRUE,
+    upper_is_strict = TRUE
+  )
+  assertive::assert_is_a_number(digits)
+  assertive::assert_all_are_greater_than_or_equal_to(digits, 0)
+
+
   n_pox <- choose(sum(mat), 2)
-  N <- sum(mat)
-  p = 0
-  num1 <- cell <- numeric(nrow(mat)*ncol(mat))
+  N     <- sum(mat)
+  p     <- 0
+
+  num1 <- cell <- numeric(nrow(mat) * ncol(mat))
   num2 <- numeric(ncol(mat))
   num3 <- numeric(nrow(mat))
+
   for (i in 1:nrow(mat)){
     for (j in 1:ncol(mat)){
       p <- p + 1
@@ -56,36 +75,55 @@ ARI_contingency <- function(mat, alpha){
       cell[[p]] <- mat[i,j]
     }
   }
-  a <- (sum(cell^2)-N)/2
-  b <- (sum(rowSums(mat)^2)-(sum(cell^2)))/2
-  c <- (sum(colSums(mat)^2)-(sum(cell^2)))/2
-  d <- (sum(cell^2)+N^2-sum(rowSums(mat)^2)-sum(colSums(mat)^2))/2
 
-  exp <- (sum(num2)*sum(num3)/n_pox)
+  a <- (sum(cell^2) - N) / 2
+  b <- (sum(rowSums(mat)^2) - (sum(cell^2))) / 2
+  c <- (sum(colSums(mat)^2) - (sum(cell^2))) / 2
+  d <- (sum(cell^2) + N^2 - sum(rowSums(mat)^2) - sum(colSums(mat)^2)) / 2
+
+  exp <- sum(num2) * sum(num3) / n_pox
   num <- sum(num1) - exp
-  den <- 0.5*(sum(num2)+sum(num3))-exp
+  den <- 0.5 * (sum(num2) + sum(num3)) - exp
 
-  e <- 2*sum(rowSums(mat)^2)-(N+1)*N
-  f <- 2*sum(colSums(mat)^2)-(N+1)*N
-  g <- 4*sum(rowSums(mat)^3)-4*(N+1)*sum(rowSums(mat)^2)+(N+1)^2*N
-  h <- N*(N-1)
-  i <- 4*sum(colSums(mat)^3)-4*(N+1)*sum(colSums(mat)^2)+(N+1)^2*N
+  e <- 2 * sum(rowSums(mat)^2) -
+       (N + 1) * N
 
-  var_ad <- 1/16*(2*N*(N-1)-
-                    ((e*f)/(N*(N-1)))^2+
-                    (4*(g-h)*(i-h))/(N*(N-1)*(N-2)))+
-    1/16*(((e^2-4*g+2*h)*(f^2-4*i+2*h))/(N*(N-1)*(N-2)*(N-3)))
+  f <- 2 * sum(colSums(mat)^2) -
+       (N + 1) * N
 
-  var_ARI <- (n_pox^2*var_ad)/((n_pox^2-((a+b)*(a+c)+(b+d)*(c+d)))^2)
+  g <- 4 * sum(rowSums(mat)^3) -
+       4 * (N + 1) * sum(rowSums(mat)^2) +
+       (N + 1)^2 * N
+
+  h <- N * (N - 1)
+
+  i <- 4 * sum(colSums(mat)^3) -
+       4 * (N + 1) * sum(colSums(mat)^2) +
+       (N + 1)^2 * N
+
+  var_ad <- 1/16 * (
+              2 * N * (N - 1) -
+              ((e * f) / (N * (N - 1)))^2 +
+              (4 * (g - h) * (i - h)) / (N * (N - 1) * (N - 2))
+            ) +
+            1/16 * (
+              ((e^2 - 4 * g + 2 * h) * (f^2 - 4 * i + 2 * h)) /
+              (N * (N - 1) * (N - 2) * (N - 3))
+            )
+
+  var_ARI <- (n_pox^2 * var_ad) /
+             (
+               (n_pox^2 - ((a + b) * (a + c) + (b + d) * (c + d)))^2
+             )
 
   ARI <- num/den
-  LL <- ARI-stats::qnorm(p=(1-alpha/2))*sqrt(var_ARI)
-  UL <- ARI+stats::qnorm(p=(1-alpha/2))*sqrt(var_ARI)
+  LL <- ARI - stats::qnorm(p = (1 - alpha / 2)) * sqrt(var_ARI)
+  UL <- ARI + stats::qnorm(p = (1 - alpha / 2)) * sqrt(var_ARI)
 
-  c("AdjustedRandIndex" = round(ARI, digits = 2),
-        "CI"   = paste(round(LL, digits = 2),
-                       round(UL, digits = 2),
-                       sep = "; "))
-
+  c(
+    "ari"     = round(ARI, digits = digits),
+    "ci_low"  = round(LL,  digits = digits),
+    "ci_high" = round(UL,  digits = digits)
+  )
 }
 
