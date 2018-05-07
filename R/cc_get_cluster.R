e6e0a263c44e58f220f03ce54daa07a82c0f4fa2
Corrado Lanera (xps) <corrado.lanera@gmail.com>
2018-05-07 11:56:36 +0200

    issue #15

--- R/cc_get_cluster.R
+++ R/cc_get_cluster.R
@@ -1,12 +1,24 @@
 #' Provides the vector of clusters' ID to which each element belong to.
 #'
-#' @param cluster_list list of clustered elements
-#' @param n_elem total number of elements clustered
+#' @param x list of clustered elements or a \code{crossclustering} object
+#' @param ... furter input for the function (e.g. total number of elements
+#'   clustered if it is not provided a crosscustering object)
 #'
 #' @return An integer vector of clusters to which the elements belong (`1`
 #'         for the outliers, ID + 1 for the others).
+#' @export
+cc_get_cluster <- function(x, ...) {
+  UseMethod('cc_get_cluster', x)
+}
+
+
+
+#' @param cluster_list list of clustered elements
+#' @param n_elem total number of elements clustered
 #'
 #' @export
+#'
+#' @describeIn cc_get_cluster default method for \code{\link{cc_get_cluster}}.
 #'
 #' @examples
 #' library(CrossClustering)
@@ -26,7 +38,7 @@
 #' )
 #'
 #' ### cc_get_cluster
-#' cc_get_cluster(toyres, attr(toyres, 'n_total'))
+#' cc_get_cluster(toyres[], 7)
 #'
 #' @author
 #' Paola Tellaroli, <paola [dot] tellaroli [at] unipd [dot] it>;;
@@ -39,7 +51,7 @@
 #' Estimation of the Number of Clusters. PLoS ONE 11(3):   e0152333.
 #' doi:10.1371/journal.pone.0152333
 
-cc_get_cluster <- function(cluster_list, n_elem) {
+cc_get_cluster.default <- function(cluster_list, n_elem) {
 
   assertive::assert_is_list(cluster_list)
   assertive::assert_is_a_number(n_elem)
@@ -68,4 +80,20 @@ cc_get_cluster <- function(cluster_list, n_elem) {
   res[elements] <- clustered_cluster + 1L
   res[outliers] <- 1L
   res
+}
+
+
+#' @param crossclustering an object of class crossclustering
+#'
+#' @describeIn cc_get_cluster automatically extract inputs from a \code{crossclustering} object
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ### cc_get_cluster directly from a crossclustering object
+#' cc_get_cluster(toyres)
+cc_get_cluster.crossclustering <- function(crossclustering) {
+  assertive::assert_is_inherited_from(crossclustering, 'crossclustering')
+  cc_get_cluster.default(crossclustering, attr(crossclustering, 'n_total'))
 }
